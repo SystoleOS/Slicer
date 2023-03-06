c017dd37fc2dd14b2f8c53e0918182ae4f7163ed
Rafael Palomar <rafael.palomar@ous-research.no>
2023-03-06 08:27:23 +0100

    ENH: Disable pull of git submodules for RapidJSON

--- SuperBuild/External_RapidJSON.cmake
+++ SuperBuild/External_RapidJSON.cmake
@@ -1,4 +1,3 @@
-
 set( proj RapidJSON )
 
 # Set dependency list
@@ -38,6 +37,8 @@ if(NOT DEFINED ${proj}_DIR AND NOT Slicer_USE_SYSTEM_${proj})
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${Slicer_${proj}_GIT_REPOSITORY}"
     GIT_TAG "${Slicer_${proj}_GIT_TAG}"
+    GIT_SUBMODULES_RECURSE 0
+    GIT_SUBMODULES ""
     SOURCE_DIR ${EP_SOURCE_DIR}
     BINARY_DIR ${EP_BINARY_DIR}
     CMAKE_CACHE_ARGS
