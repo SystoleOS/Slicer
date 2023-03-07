740dc236ce82854e0991a23b2dc8cb475cccce62
Rafael Palomar <rafael.palomar@ous-research.no>
2023-03-07 15:17:05 +0100

    ENH: Add python support

--- Base/Python/mrml.py
+++ Base/Python/mrml.py
@@ -1,7 +1,13 @@
 """ This module loads all the classes from the MRML library into its
 namespace."""
 
-from MRMLCLIPython import *
+# HACK Ideally constant from vtkSlicerConfigure should be wrapped,
+#      that way the following try/except could be avoided.
+try:
+    from MRMLCLIPython import *
+except:
+    pass
+
 from MRMLCorePython import *
 from MRMLDisplayableManagerPython import *
 from MRMLLogicPython import *
